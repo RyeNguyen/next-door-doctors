39fd8ed5fdc6c93e1c9722bc7bd1019c2e8c74b9
Minh Nguyen <minhiq149@gmail.com>
2022-08-10 16:58:26 +0700

    create all tables for the database

--- src/models/doctor_clinic_specialty.js
+++ src/models/doctor_clinic_specialty.js
@@ -3,7 +3,7 @@ const {
     Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class User extends Model {
+    class DoctorClinicSpecialty extends Model {
         /**
          * Helper method for defining associations.
          * This method is not a part of Sequelize lifecycle.
@@ -13,19 +13,13 @@ module.exports = (sequelize, DataTypes) => {
             // define association here
         }
     };
-    User.init({
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
-        firstName: DataTypes.STRING,
-        lastName: DataTypes.STRING,
-        address: DataTypes.STRING,
-        gender: DataTypes.BOOLEAN,
-        roleType: DataTypes.STRING,
-        roleKey: DataTypes.STRING,
-        image: DataTypes.STRING
+    DoctorClinicSpecialty.init({
+        doctorId: DataTypes.INTEGER,
+        clinicId: DataTypes.INTEGER,
+        specialtyId: DataTypes.INTEGER
     }, {
         sequelize,
-        modelName: 'User',
+        modelName: 'DoctorClinicSpecialty',
     });
-    return User;
+    return DoctorClinicSpecialty;
 };
--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -3,9 +3,14 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'Minh',
+      lastName: 'Nguyen',
+      address: 'Hanoi',
+      gender: 1,
+      roleType: 'ROLE',
+      roleKey: 'R1',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
