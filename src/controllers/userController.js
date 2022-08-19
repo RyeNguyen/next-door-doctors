1208c01b266850ab2b62ef6daabef526b74cdbc6
Minh Nguyen <minhiq149@gmail.com>
2022-08-19 22:56:00 +0700

    create login api at the backend

---
+++ src/controllers/userController.js
@@ -0,0 +1,25 @@
+import userService from '../services/userService';
+
+const handleLogin = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (!email || !password) {
+        return res.status(500).json({
+            errCode: 1,
+            message: 'Missing input parameter(s)!!'
+        })
+    }
+
+    const userData = await userService.handleUserLogin(email, password);
+
+    return res.status(200).json({
+        errCode: userData.errCode,
+        message: userData.message,
+        user: userData ? userData.user : {}
+    });
+}
+
+module.exports = {
+    handleLogin
+}
---
+++ src/services/userService.js
@@ -0,0 +1,66 @@
+import bcrypt from 'bcrypt';
+import db from '../models/index';
+
+const handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const userData = {};
+            const isExist = await checkUserEmail(email);
+
+            if (isExist) {
+                //user already exist
+                //compare password
+                const user = await db.User.findOne({
+                    where: {email: email},
+                    attributes: ['email', 'roleId', 'password'],
+                    raw: true
+                })
+
+                if (user) {
+                    const check = await bcrypt.compare(password, user.password);
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.message = 'OK';
+                        delete user.password;
+                        userData.user = user;
+                    } else {
+                        userData.errCode = 3;
+                        userData.message = 'Passwords do not match!!';
+                    }
+                } else {
+                    userData.errCode = 2;
+                    userData.errMessage = `User not found!!`;
+                }
+            } else {
+                userData.errCode = 1;
+                userData.errMessage = `Your email is not existed in out system. Please try other email!`;
+            }
+
+            resolve(userData);
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+const checkUserEmail = (userEmail) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const user = await db.User.findOne({
+                where: { email: userEmail }
+            });
+
+            if (user) {
+                resolve(true);
+            }
+
+            resolve(false);
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+module.exports = {
+    handleUserLogin
+};
